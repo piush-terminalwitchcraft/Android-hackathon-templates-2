93159a0549b11a3f7e77ef854d5356a63ac99757
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2022-03-06 11:29:43 +0530

    Added Drawer 😀😀

--- app/src/main/java/com/example/androidhackathontemplates/MainActivity.kt
+++ app/src/main/java/com/example/androidhackathontemplates/MainActivity.kt
@@ -3,17 +3,31 @@ package com.example.androidhackathontemplates
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import np.com.susanthapa.curved_bottom_navigation.CbnMenuItem
 
 class MainActivity : AppCompatActivity() {
 
-
-
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var bottomNavigation: np.com.susanthapa.curved_bottom_navigation.CurvedBottomNavigationView
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerNavigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottom_nav)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+        drawerLayout = findViewById(R.id.drawer_layout)
+        drawerNavigationView = findViewById(R.id.nav_drawer)
 
         val menuItems = arrayOf(
             CbnMenuItem(
@@ -38,8 +52,8 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        bottomNavigation = findViewById(R.id.bottom_nav)
         bottomNavigation.setMenuItems(menuItems,2)
+//        bottomNavigation.setupWithNavController(navController)
 
     }
 }
